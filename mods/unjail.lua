7af327329d247d4f3d1af39332f3f46fbb10e4bb
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2022-06-02 03:45:11 +0300

    Create unjail.lua

---
+++ mods/unjail.lua
@@ -0,0 +1,15 @@
+-- from incredible-gmod.ru with <3
+-- https://github.com/Be1zebub/GMD-Mods/blob/master/mods/unjail.lua
+-- источник: https://forum.gm-donate.ru/t/item-dlya-platnogo-unjail-vyjti-iz-dzhajla/899/2
+
+IGS("Побег из jail", "unjail")
+:SetPrice(100)
+:SetDescription("Админ наказал вас и поставил в угол?\nС помощью этой услуги вы можете сбежать из jail.\nТеперь правила сервера не являются помехой для вас :D")
+:SetCanActivate(function(ply)
+    if not ply.jail then
+        return "Вы сейчас не в jail! Зачем вам активировать этот предмет прямо сейчас?"
+    end
+end)
+:SetOnActivate(function(ply)
+    RunConsoleCommand("ulx", "jail", ply:SteamID(), "1", "true")
+end)
